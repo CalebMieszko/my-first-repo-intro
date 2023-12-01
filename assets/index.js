5000b01eae0d4fad4549fbb3b110c43daec3c8db
Caleb Mieszko <calebmieszko@gmail.com>
2023-11-30 21:32:59 -0800

    Showing the class that they don't have to use the old git commands for this if they don't want to!

---
+++ assets/index.js
@@ -0,0 +1,23 @@
+$("#fibo-logo").fadeIn(3000);
+
+// fibonacci's fade in ends here //
+
+$("#show").click(function() {
+  $(".contained").slideDown(600);               
+});
+
+$("#hide").click(function() {
+  $(".contained").fadeOut(600);               
+});
+
+// quarterly menu ends here //
+
+  $(".img-dis").fadeIn(4000);
+
+// maths favorite fade in ends here //
+ 
+$("#nachos").fadeIn(4000);
+$(".hrs-bg").fadeIn(4000);
+
+
+//hours and location article ends here//
